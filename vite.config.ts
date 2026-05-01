3a39f2dcf8ac43b8a9199380982bc6f4fa6927bd
kilo <kilo@test.com>
2026-05-01 15:09:59 +0800

    fix: 移除 MS Clarity 插件

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,8 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
-import MsClarity from "vite-plugin-ms-clarity";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const MS_CLARITY_ID = process.env.MS_CLARITY_ID || '';
   return {
     base: mode === 'development' ? '' : '/tiao-hao-gua/',
     server: {
@@ -22,12 +19,7 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       vue(), vueJsx(),
-      basicSsl(),
-      MsClarity({
-        id: MS_CLARITY_ID,
-        enableInDevMode: false,
-        injectTo: 'body'
-      })
+      basicSsl()
     ],
     resolve: {
       alias: [
